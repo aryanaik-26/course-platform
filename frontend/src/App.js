be8d5f1831ac7720ab15e23d23e0ef8fab26249a
Arya <205393323+aryanaik-26@users.noreply.github.com>
2026-07-14 14:59:13 +0530

    Update App.js after merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-HEAD
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,111 +6,40 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import MentorProfile from "./pages/MentorProfile";
-import Requests from "./pages/Requests";
 import Dashboard from "./pages/Dashboard";
 import Explore from "./pages/Explore";
+import MentorProfile from "./pages/MentorProfile";
+import Requests from "./pages/Requests";
 
 import "./App.css";
 
-
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <div className="App">
-
-
         <Navbar />
 
-
         <Routes>
-
-
-          {/* Home Page */}
-          <Route
-            path="/"
-            element={<Home />}
-          />
-
+          {/* Home */}
+          <Route path="/" element={<Home />} />
 
           {/* Authentication */}
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-
-
-          <Route
-            path="/signup"
-            element={<Signup />}
-          />
-
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
           {/* Main Pages */}
-          <Route
-            path="/dashboard"
-            element={<Dashboard />}
-          />
-
-
-          <Route
-            path="/explore"
-            element={<Explore />}
-          />
-
-
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/explore" element={<Explore />} />
 
           {/* Mentor */}
-          <Route
-            path="/mentor/:id"
-            element={<MentorProfile />}
-          />
+          <Route path="/mentor/:id" element={<MentorProfile />} />
 
-
-          <Route
-            path="/requests"
-            element={<Requests />}
-          />
-
-
-
+          {/* Requests */}
+          <Route path="/requests" element={<Requests />} />
         </Routes>
-
-
       </div>
-
-
     </BrowserRouter>
-
-
-import { useState } from "react";
-import useAuth from "./auth/useAuth";
-
-import LoginUI from "./pages/LoginUI";
-import SignupUI from "./pages/SignupUI";
-import Dashboard from "./pages/Dashboard";
-
-function App() {
-  const user = useAuth();
-  const [isSignup, setIsSignup] = useState(false);
-
-  // 🔥 FIXED LOGIC
-  if (user === undefined) return <h2>Loading...</h2>;
-
-  if (user) return <Dashboard user={user} />;
-
-  return isSignup ? (
-    <SignupUI switchToLogin={() => setIsSignup(false)} />
-  ) : (
-    <LoginUI switchToSignup={() => setIsSignup(true)} />
- 
   );
-
 }
 
-
-export default App
+export default App;
